fb683b56de25ce68f8bf14606be60e3c3bbea575
Valentina Contreras <valentinacr1309@gmail.com>
2025-05-04 17:09:04 -0500

    Carrusel

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = form.elements["email"].value;
+  const password = form.elements["password"].value;
+
+  console.log(email);
+  console.log(password);
+
+  const result = `${email}, bienvenido a nuestra plataforma`;
+  alert(result);
+});
